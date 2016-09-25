bfe1c45aadbf9bd17fdd2cca6701206c17617fab
Seth Hanson <hansonse17@grinnell.edu>
2016-09-25 14:49:02 +0000

    complete TDD

--- app.rb
+++ app.rb
@@ -2,7 +2,7 @@ require 'sinatra'
 
 class MyApp < Sinatra::Base
    get '/' do
-      "<!DOCTYPE html><html><head></head><body><h1>Goodbuy Worldy!</h1></body></html>"
+      "<!DOCTYPE html><html><head></head><body><h1>Goodbuy Wald!</h1></body></html>"
    end
 end
 
